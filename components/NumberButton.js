5a2a71dbbab7cb771344f04390bf76c6cddfb942
Trey Huffine <treyhuffine@gmail.com>
2015-10-25 21:30:12 -0700

    Remove log statements

--- components/NumberButton.js
+++ components/NumberButton.js
@@ -6,11 +6,9 @@ export default class NumberButton extends React.Component {
     this.handleClickEvent = this.handleClickEvent.bind(this);
   }
   handleClickEvent() {
-    console.log('CLICK PROPS', this.props);
     this.props.onLocalClick(Number(this.props.buttonVal));
   }
   render() {
-    console.log('PROPS', this.props);
     return (
       <button className="number-button" onClick={this.handleClickEvent}>
         +{this.props.buttonVal}
